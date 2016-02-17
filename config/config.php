7d5a932a3df1a76f9a36661d3e0609c40f8ecc34
Jérémy BERNARD <jeremybernard1984@users.noreply.github.com>
2016-02-17 16:33:50 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 return array(
-    "db_user" => "dbo601175712",
-    "db_pass" => "Sdk56six",
-    "db_host" => "db601175712.db.1and1.com",
-    "db_name" => "dbo601175712"
+    "db_user" => "",
+    "db_pass" => "",
+    "db_host" => "",
+    "db_name" => ""
 );
